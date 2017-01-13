cbc4a47d3ecbd27626b770ca899664b9cbf96ebf
Philip <pfranchi@penguinrandomhouse.com>
2017-01-13 12:55:48 -0500

    Fixed p5.pixels bug, still no image displayed

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 const KGROUPS = 8;
-const MAX_ITR = 20;
+const MAX_ITR = 2;
 const MAX_INT = 999;
 
 var canvas;
@@ -7,92 +7,100 @@ var canvas;
 //Image user preloaded, used to determine the size of the canvas
 var img;
 
+function debug(t){console.log(t)}
+
 function preload(){
-    img = loadImage("assets/test.jpg",640,480)
-    
+    img = loadImage("assets/test.jpg",64,64)
 }
 
 function getRandomColor(){
     return color(Math.random(0,256),Math.random(0,256),Math.random(0,256));
 }
 
-function createNexi(){
-    var labels = new Array(KGROUPS);
+function createNexuses(){
+    var ret = new Array(KGROUPS);
     for(var c = 0; c < KGROUPS; c++){
-        labels[c] = getRandomColor()
+        ret [c] = getRandomColor()
     }
-    return labels
+    return ret 
 }
 function calcColorDist(nexus, pixel){
-    /*var h = Math.pow(hue(nexus) - hue(pixel),2)
+    var h = Math.pow(hue(nexus) - hue(pixel),2)
     var s = Math.pow(saturation(nexus) - saturation(pixel),2)
     var b = Math.pow(brightness(nexus) - brightness(pixel),2)
-    return Math.sqrt(h + s + b);*/
-    var h = hue(nexus) - hue(pixel)
-    h = h*h
-    var s = saturation(nexus) - saturation(pixel)
-    s = s*s
-    var b = brightness(nexus) - brightness(pixel)
-    b = b*b
-    return Math.sqrt(h + s + b)
+    return Math.sqrt(h + s + b);
 }
-function relabelPixels(nexi, pixels){
-   var ret = new Array(img.width*img.height) 
-    for(var p = 0; p < pixels.length; p++){
-        var curPixel = pixels[p]
+function relabelPixels(nexuses, pixels){
+    debug("relabeling pixels")
+    //Each index will store on Color Object
+    var ret = new Array(img.width*img.height) 
+    //Pixel values are stored as [r,g,b,a,r,g,b,a], so the color of (0,0) is actually index 0-3
+    for(var p = 0; p < pixels.length; p+=4){
+        
+        var curPixel = color(pixels[p], pixels[p+1], pixels[p+2], pixels[p+3])  
         var bestNexus = 0
         var bestDist = MAX_INT
-        for(var n = 0; n < nexi.length; n++){
-            var newDist = calcColorDist(nexi[n], pixels)
+
+        for(var n = 0; n < nexuses.length; n++){
+            var newDist = calcColorDist(nexuses[n], curPixel)
             if(newDist < bestDist){
                 bestNexus = n
                 bestDist = newDist
             }
         }
-        ret[p] = bestNexus
-    }
-   return ret
+        ret[p/4] = bestNexus
+    }   
+    return ret
 }
 
-function averageLabels(nexi, pixels, labels){
-    var ret = new Array(nexi.length)
-    for(var n = 0; n < nexi.length; n++){
-        var count = 0
-        var h = 0
-        var s = 0
-        var b = 0
-        for(var p = 0; p < labels.length; p++){
-            if(p == n){
-                count++
-                h+=hue(pixels[p])
-                s+=saturation(pixels[p])
-                b+=brightness(pixels[p])
+function averageLabels(nexuses, pixels, labels){
+    debug("AVERAGING LABELS")
+    var ret = new Array(nexuses.length)
+        for(var n = 0; n < nexuses.length; n++){
+            var count = 0
+            var h = 0
+            var s = 0
+            var b = 0
+            for(var p = 0; p < pixels.length; p+=4){
+                var curColor = color(pixels[p], pixels[p+1], pixels[p+2], pixels[p+3])
+                if(curColor == nexuses[n]){
+                    count++
+                    h+=hue(curColor)
+                    s+=saturation(curColor)
+                    b+=brightness(curColor)
+                }
             }
+            ret[n] = color(h/count,s/count,b/count)
         }
-        ret[n] = color(h/count,s/count,b/count)
-    }
     return ret
 }
 
 function kmeans(img){
-    var nexi = createNexi()
-    
+    debug("Begin KMEANS")
+    var nexuses = createNexuses()
     var ret = new p5.Image(img.width,img.height)
     var labels = new Array(img.width*img.height)
-    for(var i = 0; i < MAX_ITR; i++){
-        
-        //relabel pixels based on nexi
-        labels = relabelPixels(nexi, labels)
-        //Remap nexi 
-        nexi = averageLabels(nexi, img.pixels, labels)
-    }
 
+    img.loadPixels()
+
+    for(var i = 0; i < MAX_ITR; i++){
+
+        //relabel pixels based on nexuses
+        labels = relabelPixels(nexuses, img.pixels)
+        //Remap nexuses 
+        nexuses = averageLabels(nexuses, img.pixels, labels)
+        }
+    for(var x = 0; x < labels.length; x++){
+       ret.set(x%img.width,Math.floor(x/img.width), nexuses[labels[x]])
+    }
+    console.log(red(ret.get(20,100)))
+    ret.updatePixels()
     return ret
 }
 
 function setup() {
     canvas = createCanvas(img.width,img.height)
-    img = kmeans(img);    
+    img = kmeans(img)    
     noLoop()
 }
 
